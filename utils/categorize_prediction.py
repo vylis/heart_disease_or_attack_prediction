ff42d4ca5634aee34624e55c5cd8a2bdcebd26c4
vylis <vylis@vylis.local>
2024-03-19 15:44:55 +0100

    fix categorize prediction based on european SCORE

--- utils/categorize_prediction.py
+++ utils/categorize_prediction.py
@@ -11,11 +11,8 @@ def categorize_prediction(predictions, data):
             )
         ):
             categories.append("Risque très élevé")
-        elif (
-            data_dict["Diabetes"] == 2
-            or data_dict["HighBP"] == 1
-            or data_dict["HighChol"] == 1
-            or (data_dict["Diabetes"] == 1 and data_dict["Age"] > 1)
+        elif data_dict["Diabetes"] == 2 or (
+            data_dict["Diabetes"] == 1 and data_dict["Age"] > 1
         ):
             categories.append("Risque élevé")
         elif prediction < 0.1:
@@ -24,6 +21,6 @@ def categorize_prediction(predictions, data):
             categories.append("Risque modéré")
         elif 0.5 <= prediction < 0.85:
             categories.append("Risque élevé")
-        elif prediction >= 85:
+        elif prediction >= 0.85:
             categories.append("Risque très élevé")
     return categories
